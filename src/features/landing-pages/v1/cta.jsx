45a4d3dd1323f3dcc7bafc22141d8f980c0fde88
Mark Tellez <mark@devmentor.live>
2022-11-01 16:43:03 -0600

    new price

--- src/features/landing-pages/v1/cta.jsx
+++ src/features/landing-pages/v1/cta.jsx
@@ -8,7 +8,7 @@ export default function CTA({}) {
       <div className="sm:flex items-center">
         <div className="sm:w-1/2 flex flex-col items-end">
           <h3 className="text-[60px] font-book uppercase">Get started today</h3>
-          <p className="text-3xl">1 on 1 sessions are $80/hr usd</p>
+          <p className="text-3xl">1 on 1 sessions are $120/hr usd</p>
           <p className="text-xl font-light">
             schedule and use and pay for only what you need.
           </p>
