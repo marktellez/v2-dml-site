324b41e97189647c954bea5dc13efc23650de891
Mark Tellez <mark@devmentor.live>
2022-11-07 21:00:05 -0600

    fixed footer links

--- src/ui/page/footer.jsx
+++ src/ui/page/footer.jsx
@@ -10,16 +10,20 @@ export default function Footer({}) {
         <div className="flex justify-around my-16">
           <div className="w-1/2 text-center">
             <div>Join the conversation!</div>
-            <button className=" bg-indigo-600 p-4 rounded-xl">
+            <a
+              className=" bg-indigo-600 p-4 rounded-xl"
+              href="https://bit.ly/3dh0r8D">
               <DiscordIcon className="w-16 h-16" />
-            </button>
+            </a>
           </div>
           <div className="w-1/2 text-center">
             <div>
               <div>Watch and learn!</div>
-              <button className="w-32 h-32">
+              <a
+                className="w-32 h-32 "
+                href="https://youtube.com/@devmentorpro">
                 <YoutubeIcon className="w-16 h-16" />
-              </button>
+              </a>
             </div>
           </div>
         </div>
